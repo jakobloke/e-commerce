37f3440483b4de8b202e44a8dc4aaa3fe7d30ada
jakobloke <61665831+jakobloke@users.noreply.github.com>
2020-06-08 11:38:23 -0700

    created models framework

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const routes = require('./routes');
+const seqelize = require('./config/connectioin');
 // import sequelize connection
 
 const app = express();
@@ -11,6 +12,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+seqelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`Now listening on port ${PORT}!`));
 });
